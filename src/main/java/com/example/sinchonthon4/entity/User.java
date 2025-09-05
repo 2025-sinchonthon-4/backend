5848004adad223d773966d05e6171f2c95a1dad9
art1factia <137972596+art1factia@users.noreply.github.com>
2025-09-06 01:59:15 +0900

    add getQuizListByCategory

--- src/main/java/com/example/sinchonthon4/entity/User.java
+++ src/main/java/com/example/sinchonthon4/entity/User.java
@@ -60,6 +60,11 @@ public class User extends BaseTimeEntity implements Serializable {
     @Column(nullable = false)
     private LocalDateTime updatedAt = LocalDateTime.now();
 
+    @ElementCollection
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Set<Category> interests = new HashSet<>();
+
     // 레벨, 경험치 업데이트를 위한 편의 메서드 (추후 구현)
     public void addExp(int amount) {
         this.exp += amount;
--- src/main/java/com/example/sinchonthon4/repository/QuizRepository.java
+++ src/main/java/com/example/sinchonthon4/repository/QuizRepository.java
@@ -1,9 +1,13 @@
 package com.example.sinchonthon4.repository;
 
+import com.example.sinchonthon4.entity.Category;
 import com.example.sinchonthon4.entity.Quiz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
@@ -11,4 +15,6 @@ public interface QuizRepository extends JpaRepository<Quiz, Long> {
     // H2 DB에서는 RAND() 대신 RANDOM() 사용
     @Query(value = "SELECT * FROM quiz ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
     Quiz findRandomQuiz();
+
+    List<Quiz> findByCategoryIn(Collection<Category> categories);
 }
