142e7a5ab2746564d7def5a25986ac4d1d879981
art1factia <137972596+art1factia@users.noreply.github.com>
2025-09-06 01:40:44 +0900

    merge

---
+++ src/main/java/com/example/sinchonthon4/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.sinchonthon4.repository;
+
+import com.example.sinchonthon4.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
